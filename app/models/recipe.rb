941dd951e3442ddf6e86cb59947ef84456c95006
Mikelanne <mikelanne.wood@gmail.com>
2021-02-18 11:21:36 -0500

    adding associations

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,4 @@
 class Recipe < ApplicationRecord
+    belongs_to :CountryOfOrigin
+    belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+    has_many :recipes
+    has_many :CountryOfOrigins, through: :recipes
 end
