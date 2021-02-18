5120bf325ffebe1b7a90c36c49822c360d72431a
Mikelanne <mikelanne.wood@gmail.com>
2021-02-18 15:35:41 -0500

    adding a where scope method to recipes

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,6 +2,7 @@ class Recipe < ApplicationRecord
     belongs_to :country_of_origin
     belongs_to :user
     validates :name, :ingredients, :directions, :country_of_origin_id, :user_id, presence: true
+    scope :dinner, -> { where(meal_type: "dinner")}
 
     def self.ordered_by_skill_level
         self.order(skill_level: :desc)
