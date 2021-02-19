de6458f7e4085b0a2fb362346e11c9335802e805
Mikelanne <mikelanne.wood@gmail.com>
2021-02-19 13:45:45 -0500

    login ability functional

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,16 @@
 class ApplicationController < ActionController::Base
+    helper_method :current_user
 
     def home
     end
 
     def current_user
+      session[:name]
     end
-
-
+  
+    private
+  
+    def require_login
+      redirect_to :controller => 'sessions', :action => 'new' unless current_user
+    end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,12 +5,19 @@ class SessionsController < ApplicationController
     end 
 
     def create
-        @user = User.find_by(name: params[:user][:name])
-        return head(:forbidden) unless @user.authenticate(params[:user][:password])
-        session[:user_id] = @user.id 
+        user = User.find_by(username: params[:username])
+        if user && user.authenticate(params[:password])
+            session[:user_id] = user.id
+            redirect_to '/' # will want to redirect to user show page once created
+        else
+            flash[:message] = "Invalid credentials. Please try again."
+            redirect_to '/login'
+        end
     end 
 
     def destroy
+        session.delete[:user_id]
+        redirect_to '/login'
     end
 
 end
