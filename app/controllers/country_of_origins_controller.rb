1e88543369c9b749106ec2016b8bff8d8a9c95d7
Mikelanne <mikelanne.wood@gmail.com>
2021-02-18 19:47:27 -0500

    adding index views and controller action to countries

--- app/controllers/country_of_origins_controller.rb
+++ app/controllers/country_of_origins_controller.rb
@@ -1,6 +1,7 @@
 class CountryOfOriginsController < ApplicationController
 
     def index
+        @countries = CountryOfOrigin.all
     end
 
     def show
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'application#home'
   resources :recipes
-  resources :country_of_origins
+  resources :country_of_origins, as: :countries
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
