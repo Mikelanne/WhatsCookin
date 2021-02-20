d81e808d814a4f6d579e1d23416362dd1be9d9d7
Mikelanne <mikelanne.wood@gmail.com>
2021-02-20 18:18:41 -0500

    editing new recipe form so you can only select a country outside of nested route.

--- app/helpers/recipes_helper.rb
+++ app/helpers/recipes_helper.rb
@@ -7,4 +7,12 @@ module RecipesHelper
             "Recipe Library"
         end
     end
+
+    def display_country_fields(country, f)
+        if country
+            f.hidden_field :country_of_origin_id
+        else
+            render partial: "country_select", locals: {f: f}
+        end
+    end
 end
